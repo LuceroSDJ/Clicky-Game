9d97cdf6a610a2aad48a53a7235877a6036304c3
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-05-04 23:49:05 -0500

    updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import PaintingCard from "./components/PaintingCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import paintings from "./paintings.json";
 
 function App() {
+  // Setting this.state.paintings to the paintings json array
+  // state = {
+  //   paintings
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Paintings List</Title>
+      {this.state.paintings.map(painting => (
+        <PaintingCard
+          id={painting.id}
+          key={painting.id}
+          image={painting.image}
+        />
+      ))}
+    </Wrapper>
   );
 }
 
