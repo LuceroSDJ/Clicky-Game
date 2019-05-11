3696a1341343c9296304ea751942c3f528645ef9
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-05-11 01:59:53 -0500

    Updates

---
+++ precache-manifest.f0301d55831186f515663e3c1d6e31f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83fa3511968306381fcd1f8f049589a",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "ff43d076af394fef7b84",
+    "url": "/Clicky-Game/static/css/main.07428fbe.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/Clicky-Game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "ff43d076af394fef7b84",
+    "url": "/Clicky-Game/static/js/main.fbad461e.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
